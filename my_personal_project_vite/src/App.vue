2f1f8ac176811c4c8f8f6edcd2781e2853ca5e6a
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-03-20 14:16:42 +0100

    add export default, data and return with dynamic title for h1

--- my_personal_project_vite/src/App.vue
+++ my_personal_project_vite/src/App.vue
@@ -1,8 +1,15 @@
 <script>
+export default {
+  data() {
+    return {
+      title: "Hello world"
+    }
+  }
+}
 </script>
 
 <template>
-  <h1>Hello world</h1>
+  <h1>{{ title }}</h1>
 </template>
 
 <style></style>
